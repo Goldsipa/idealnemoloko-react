29a5b06a4f10cc14b69d956ba5d992462fad5b51
Goldsipa <goldsip@gmail.com>
2018-11-07 19:19:21 +0200

    Updates

---
+++ precache-manifest.ca9149f28a2b6674c301b75c088fc763.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "15954e12289d695ce4c6d299af65da15",
+    "url": "/idealnemoloko-react/static/media/logo.15954e12.png"
+  },
+  {
+    "revision": "6272b54393e50485579f",
+    "url": "/idealnemoloko-react/static/css/main.de97186c.chunk.css"
+  },
+  {
+    "revision": "6515d2c6d2be45e686a5",
+    "url": "/idealnemoloko-react/static/js/1.6515d2c6.chunk.js"
+  },
+  {
+    "revision": "b86a6ed1e15bb0a44792",
+    "url": "/idealnemoloko-react/static/js/runtime~main.b86a6ed1.js"
+  },
+  {
+    "revision": "3f58d9717e5e9c4c72d213ef0a4f9e9d",
+    "url": "/idealnemoloko-react/static/media/vivsyane.3f58d971.png"
+  },
+  {
+    "revision": "57b8b4384c1a6ec30ad074a01ba225ac",
+    "url": "/idealnemoloko-react/static/media/grechane.57b8b438.png"
+  },
+  {
+    "revision": "6272b54393e50485579f",
+    "url": "/idealnemoloko-react/static/js/main.6272b543.chunk.js"
+  },
+  {
+    "revision": "d25055ebca7b6b4e88d9ececfd4da46b",
+    "url": "/idealnemoloko-react/static/media/splash-left.d25055eb.png"
+  },
+  {
+    "revision": "d15eddc374047e9a550e36c8e52fe51e",
+    "url": "/idealnemoloko-react/static/media/splash-right.d15eddc3.png"
+  },
+  {
+    "revision": "6741755c6f18c5159d3dd79d60ba6959",
+    "url": "/idealnemoloko-react/static/media/leaves.6741755c.png"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/idealnemoloko-react/static/media/Proxima-Nova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "8a1b91a8e5ef27b6c7ac813ef2235dd8",
+    "url": "/idealnemoloko-react/static/media/LEngineer-Regular.8a1b91a8.otf"
+  },
+  {
+    "revision": "e02146fbd9db6f16789b37a840a78cd9",
+    "url": "/idealnemoloko-react/index.html"
+  }
+];
