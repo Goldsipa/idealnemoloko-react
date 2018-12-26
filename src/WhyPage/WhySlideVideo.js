e35ea610a74f898d3d4e1345d1b690e2f0d649d3
Goldsipa <goldsip@gmail.com>
2018-12-26 17:55:33 +0200

    Changed youtube iframe to video element.

--- src/WhyPage/WhySlideVideo.js
+++ src/WhyPage/WhySlideVideo.js
@@ -1,9 +1,13 @@
 import React from 'react';
+const videoUrl = 'https://res.cloudinary.com/syplemstudio/video/upload/v1545838086/idealnemoloko/Ideal_Nemoloko_20.mp4';
 
 const WhySlideVideo = (props) => (
   <div className="swiper-slide">
     <div className="why-video">
-      <iframe src="https://www.youtube.com/embed/YbJOTdZBX1g" frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen title="idealnemoloko-video"></iframe>
+      <video controls>
+        <source src={ videoUrl } type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   </div>
 );
